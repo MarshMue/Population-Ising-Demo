0464dec85c0af8588800fa964dd9826dc24e06a9
marshallmueller <mashymue@gmail.com>
2018-12-09 15:14:56 -0500

    commented for clarity and changed variable names, also added grid demo

--- energy.py
+++ energy.py
@@ -1,30 +1,9 @@
 from scipy.sparse import identity
 import numpy as np
 
-def self_energy(x, A):
-    '''
-    x - population vector for x
-    A - adjacency matrix for graph
-    
-    return - energy score of cluster
-    '''
-    result = np.matmul(x.T, np.matmul(A + np.identity(A[0].size), x)) - np.dot(x.T, np.ones((A[0].size,1)))
-    return result[0,0]/2
-
-def sparse_self_energy(x, A):
-    '''
-    x - population vector for x
-    A - sparse adjacency matrix for graph
-    
-    return - energy score of cluster
-    '''
-    M = A + identity(A.shape[0])
-    first = np.dot(x.T, M.dot(x))
-    second = np.dot(x.T, np.ones((A.shape[0],1)))
-    result = first - second
-    return result[0,0]/2
-
-def diff_energy(x, y, A):
+# the inner product expression as defined in the CAPY paper, explicitly counts the 
+# number of edges between different populations and is approximately 2 times the explicit number of edges between the same population
+def inner_product(x, y, A):
     '''
     x - population vector for x
     y - population vector for y
@@ -35,7 +14,10 @@ def diff_energy(x, y, A):
     result = np.matmul(x.T, np.matmul(A + np.identity(A[0].size), y))
     return result[0,0]
 
-def sparse_diff_energy(x, y, A):
+# the inner product expression as defined in the CAPY paper, explicitly counts the 
+# number of edges between different populations and is approximately 2 times the explicit number of edges between the same population
+# computations done with sparse matrices for significant speedup
+def sparse_inner_product(x, y, A):
     '''
     x - population vector for x
     y - population vector for y
@@ -46,28 +28,58 @@ def sparse_diff_energy(x, y, A):
     M = A + identity(A.shape[0])
     return np.dot(x.T, M.dot(y))[0,0]
 
-def capy(x,y,A):
+# the half edge capy score, interpretible as the average of the probabilities of the type: if I'm a node of type X, what is the probability my neighbor is type X also?
+def half_capy(x,y,A):
     '''
     x - population vector for x
     y - population vector for y
     A - adjacency matrix for graph
     
-    return - the capy score
+    return - the half-edge capy score
     '''
-    xx = self_energy(x,A)
-    xy = diff_energy(x,y,A)
-    yy = self_energy(y,A)
+    xx = inner_product(x,x,A)
+    xy = inner_product(x,y,A)
+    yy = inner_product(y,y,A)
     return (xx/(xx+xy) + yy/(yy+xy))/2
 
-def sparse_capy(x,y,A):
+# the edge capy score, interpretible as the average of probabilities of the type: given that i pick an edge that touches a node of type X, what is the probability it connects two nodes of type X?
+def edge_capy(x,y,A):
+    '''
+    x - population vector for x
+    y - population vector for y
+    A - adjacency matrix for graph
+
+    return - the edge capy score
+    '''
+    xx = inner_product(x,x,A)
+    xy = inner_product(x,y,A)
+    yy = inner_product(y,y,A)
+    return (xx/(xx+2*xy) + yy/(yy+2*xy))/2
+
+
+# the half edge capy score, done with sparse matrices for faster computation
+def sparse_half_capy(x,y,A):
     '''
     x - population vector for x
     y - population vector for y
     A - sparse adjacency matrix for graph
     
-    return - the capy score
+    return - the half-edge capy score
     '''
-    xx = sparse_self_energy(x,A)
-    xy = sparse_diff_energy(x,y,A)
-    yy = sparse_self_energy(y,A)
+    xx = sparse_inner_product(x,x,A)
+    xy = sparse_inner_product(x,y,A)
+    yy = sparse_inner_product(y,y,A)
     return (xx/(xx+xy) + yy/(yy+xy))/2
+
+def sparse_edge_capy(x,y,A):
+    '''
+    x - population vector for x
+    y - population vector for y
+    A - sparse adjacency matrix for graph
+    
+    return - the edge capy score
+    '''
+    xx = sparse_inner_product(x,x,A)
+    xy = sparse_inner_product(x,y,A)
+    yy = sparse_inner_product(y,y,A)
+    return (xx/(xx+2*xy) + yy/(yy+2*xy))/2
--- ising.py
+++ ising.py
@@ -1,52 +1,76 @@
 import numpy as np
-from energy import sparse_capy
+from energy import sparse_half_capy
 class ising:
 
-	def __init__(self, repvotes, demvotes, A, beta=2**20, target=1.0, steps=100):
+	# initialize various parameters related to the simulation
+	def __init__(self, repvotes, demvotes, A, temp=2**20, target_energy=1.0, steps=100):
 		# vote totals
 		self.repvotes = repvotes
 		self.demvotes = demvotes
 
 		# temperature
-		self.beta = beta
+		self.temp = temp
 
 		# adjacency matrix
 		self.A = A
 
-		self.target = target
+		# desired energy level - between 0 and 1
+		self.target_energy = target_energy
+
+		# how long the simulation should run
 		self.steps = steps
 		
+	# setup an iterator where each step will correspond to a step in the simulation
 	def __iter__(self):
 		self.counter = 0
 		self.newrepvotes = np.array(self.repvotes)
 		self.newdemvotes = np.array(self.demvotes)
 		return self
 
+	# how the next step in the simulation is determined
 	def __next__(self):
 		'''
 		iterator to give the next state in the ising_simulation
+		Simple metropolis hastings algorithm that always accepts steps that move in the direction of the target energy
+		and accepts steps that move away from the target energy with probability based on a "temperature" parameter, and the difference in energies of the two configurations
 		'''
+
+		# stop simulation if reached number of desired steps
 		if self.counter == self.steps:
 			raise StopIteration
+
+		# otherwise increment step counter
 		self.counter += 1
+		
+		# get new proposed configurations of votes
 		prop_rep, prop_dem = self.proposal(self.newrepvotes, self.newdemvotes)
-		old_en = sparse_capy(self.newrepvotes, self.newdemvotes, self.A)
-		new_en = sparse_capy(prop_rep, prop_dem, self.A)
-		en = old_en
-		if (old_en < self.target):
-			swap = self.accept(old_en, new_en, self.beta)
+
+		# compute the old and new energies
+		old_energy = sparse_half_capy(self.newrepvotes, self.newdemvotes, self.A)
+		new_energy = sparse_half_capy(prop_rep, prop_dem, self.A)
+		energy = old_energy
+
+		# determine whether to accept or reject the proposed step
+		# how we determine if we accept depends on if we are above or below the target energy
+		if (old_energy < self.target_energy):
+			swap = self.accept(old_energy, new_energy)
 		else:
-			swap = self.accept(new_en, old_en, self.beta)
+			swap = self.accept(new_energy, old_energy)
+
+		# if we swap, assign the new data for the simulation state
 		if swap:
-			en = new_en
+			energy = new_energy
 			self.newrepvotes = prop_rep
 			self.newdemvotes = prop_dem
-		return self.newrepvotes, self.newdemvotes, en
+		
+		# return current simulation state
+		return self.newrepvotes, self.newdemvotes, energy
 
+	# tells how long the simulation is
 	def __len__(self):
 		return self.steps
 
-	def accept(self, old, new, beta):
+	def accept(self, old_energy, new_energy):
 		'''
 		metropolis hasting acceptance function for higher energies (i.e. always accept higher)
 		old - old energy score
@@ -54,14 +78,19 @@ class ising:
 
 		### swap old and new to accept lower energies
 		'''
-		if new >= old:
+		if new_energy >= old_energy:
 			return True
-		elif np.random.rand() < np.exp(-beta*(old-new)):
+		elif np.random.rand() < np.exp(-self.temp*(old_energy-new_energy)):
 			return True
 		else:
 			return False
     
 	def proposal(self, curr_rep, curr_dem):
+		'''
+		proposal function, randomly pick two different nodes and swap some number of republican and democrat voters
+		'''
+
+		# choose two nodes uniformly at random
 		idx1 = 0
 		idx2 = 0
 		new_rep = np.array(curr_rep)
@@ -70,18 +99,22 @@ class ising:
 			idx1 = np.random.randint(0,curr_rep.size-1)
 			idx2 = np.random.randint(0,curr_dem.size-1)
 		
-		# number of people to swap
+		# determine the number of people to swap
+		# must be less than or equal to the number of people of either party at the two nodes
 		max_num_swap = min(curr_rep[idx1], curr_dem[idx2])
 		if max_num_swap > 1:
 			num_swap = np.random.randint(1, max_num_swap)
 		else:
 			num_swap = 0
+
+		# swap republicans from node 1 to 2 and swap democrats from node 2 to 1
 		new_rep[idx1] -= num_swap
 		new_rep[idx2] += num_swap
 		new_dem[idx1] += num_swap
 		new_dem[idx2] -= num_swap
 		return new_rep, new_dem
 
+	# change the target energy for the simulation
 	def set_target(self, target):
 		self.target = target
 	
